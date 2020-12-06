c16c1fb07dc7a34ed4b67e8bc7eaac7e5592a372
Warrenayiss <warrenayiss@outlook.com>
2020-12-06 15:33:36 +0000

    Création de la page Build + Préparation class model Unit

---
+++ Client/Services/BananaService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattle.Client.Services
+{
+	public class BananaService : IBananaService
+	{
+		public event Action OnChange;
+		public int bananas { get; set; } = 1000;
+
+		public void EatBananas(int amount)
+		{
+			bananas -= amount;
+			BananaChanged();
+			Console.WriteLine("Bananas eaten");
+		}
+		void BananaChanged() => OnChange.Invoke();
+
+		public void AddBananas(int amount)
+		{
+			bananas += amount;
+			BananaChanged();
+		}
+	}
+}
---
+++ Client/Services/IBananaService.cs
@@ -0,0 +1,15 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattle.Client.Services
+{
+	public interface IBananaService
+	{
+		event Action OnChange;
+		int bananas { get; set; }
+		void EatBananas(int amount);
+		void AddBananas(int amount);
+	}
+}
